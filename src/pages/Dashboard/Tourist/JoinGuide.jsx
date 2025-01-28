912118335eb2dc0e327d1bf5b8094695788555db
khademul mowla <khademulmowla31eee@gmail.com>
2025-01-28 23:53:56 +0600

    updated

--- src/pages/Dashboard/Tourist/JoinGuide.jsx
+++ src/pages/Dashboard/Tourist/JoinGuide.jsx
@@ -79,21 +79,6 @@ const JoinGuide = () => {
                     </button>
                 </form>
             </div>
-
-            {/* {isSubmitted && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-                        <h2 className="text-xl font-bold">Application Successful!</h2>
-                        <p>Your request has been submitted successfully.</p>
-                        <button
-                            className="btn btn-primary mt-4"
-                            onClick={() => setIsSubmitted(false)}
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            )} */}
         </div>
     );
 };
