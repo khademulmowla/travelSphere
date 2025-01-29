1aaec1850e6d9e77f97a7365b37b3659bc747ca0
khademul mowla <khademulmowla31eee@gmail.com>
2025-01-29 19:31:42 +0600

    updated

--- src/Layout/DashBoardLayout.jsx
+++ src/Layout/DashBoardLayout.jsx
@@ -1,9 +1,13 @@
+import { Helmet } from 'react-helmet-async';
 import Sidebar from '../components/Dashboard/Sidebar/Sidebar';
 import { Outlet } from 'react-router-dom';
 
 const DashBoardLayout = () => {
     return (
         <div className='relative min-h-screen md:flex bg-white'>
+            <Helmet>
+                <title>TravelSphere | Dashboard</title>
+            </Helmet>
             {/* Left Side: Sidebar Component */}
             <Sidebar />
             {/* Right Side: Dashboard Dynamic Content */}
